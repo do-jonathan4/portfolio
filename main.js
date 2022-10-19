546cb8c37e6b004f0ed84b14b428f6d296fc6a29
do-jonathan4 <d.johnt45@gmail.com>
2022-10-19 06:16:32 -0700

    added styling, bootstrap

--- main.js
+++ main.js
@@ -4,32 +4,4 @@ const tech = document.getElementById('tech')
 const projects = document.getElementById('projects')
 const contact = document.getElementById('contact')
 
-// window.addEventListener("scroll", function() {
-//     const aboutSec = about.offsetTop;
-//     const techSec = tech.offsetTop;
-//     const divHeight = tech.offsetHeight;
-//     // executes on #about 
-//     if (window.scrollY > aboutSec && window.scrollY < techSec) {
-//         window.scrollTo({ top: (aboutSec+divHeight), behavior: 'smooth' });
-//     }
-// })
-// window.addEventListener('wheel', checkScrollDirection);
-
-// function checkScrollDirection(event) {
-//   if (checkScrollDirectionIsUp(event)) {
-//     console.log('UP');
-//   } else {
-//     console.log('Down');
-//     window.scroll({top: tech.offsetTop, behavior: 'smooth'})
-//   }
-// }
-// function checkScrollDirectionIsUp(event) {
-//     console.log(event.wheelDelta)
-//     if (event.wheelDelta) {
-//       return event.wheelDelta > 0;
-//     }
-//     return event.deltaY < 0;
-//   }
-
-about.scrollIntoView({behavior: "smooth"});
 
