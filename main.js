9ef69768a8800266eb8486ddc7964a5d5a879f0b
do-jonathan4 <d.johnt45@gmail.com>
2022-10-21 11:49:41 -0700

    edit styling for body, added scroll effect for navbar

--- main.js
+++ main.js
@@ -4,4 +4,7 @@ const tech = document.getElementById('tech')
 const projects = document.getElementById('projects')
 const contact = document.getElementById('contact')
 
-
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header')
+    header.classList.toggle('sticky', window.scrollY > 0)
+})
