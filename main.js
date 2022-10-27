9d20674d1448e6a639ba2c224af925e44487a74a
do-jonathan4 <d.johnt45@gmail.com>
2022-10-27 07:35:34 -0700

    home tab text, title padding

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const projects = document.getElementById('projects')
 const contact = document.getElementById('contact')
 const toggleButton = document.querySelector('.toggle-button')
 const navbarLinks = document.querySelector('.navbar-links')
+const openingTag = document.querySelectorAll('.opening')
+const closingTag = document.getElementsByClassName('closing')
 
 window.addEventListener('scroll', () => {
   const header = document.querySelector('header')
@@ -17,3 +19,7 @@ toggleButton.addEventListener('click', () => {
 navbarLinks.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
+
+// Object.keys(openingTag).forEach(x => openingTag[x].textContent = '<')
+// Object.keys(closingTag).forEach(x => closingTag[x].textContent = '/>')
+
