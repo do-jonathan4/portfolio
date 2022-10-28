0a7de693767d0b5276198a4b7bf6aa8b299b6e3e
do-jonathan4 <d.johnt45@gmail.com>
2022-10-28 14:17:39 -0700

    edit home tab, attempt typing animation

--- main.js
+++ main.js
@@ -3,10 +3,12 @@ const about = document.getElementById('about')
 const tech = document.getElementById('tech')
 const projects = document.getElementById('projects')
 const contact = document.getElementById('contact')
+
 const toggleButton = document.querySelector('.toggle-button')
 const navbarLinks = document.querySelector('.navbar-links')
 const openingTag = document.querySelectorAll('.opening')
 const closingTag = document.getElementsByClassName('closing')
+const homeIntro = document
 
 window.addEventListener('scroll', () => {
   const header = document.querySelector('header')
@@ -19,7 +21,3 @@ toggleButton.addEventListener('click', () => {
 navbarLinks.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
-
-// Object.keys(openingTag).forEach(x => openingTag[x].textContent = '<')
-// Object.keys(closingTag).forEach(x => closingTag[x].textContent = '/>')
-
