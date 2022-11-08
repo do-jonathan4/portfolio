35279a97cad23409575bedcc73a13602a4111538
do-jonathan4 <d.johnt45@gmail.com>
2022-11-07 20:09:44 -0800

    correct about, added icon img

--- main.js
+++ main.js
@@ -1,14 +1,7 @@
-const home = document.getElementById('home')
-const about = document.getElementById('about')
-const tech = document.getElementById('tech')
-const projects = document.getElementById('projects')
-const contact = document.getElementById('contact')
-
 const toggleButton = document.querySelector('.toggle-button')
 const navbarLinks = document.querySelector('.navbar-links')
 const openingTag = document.querySelectorAll('.opening')
 const closingTag = document.getElementsByClassName('closing')
-const homeIntro = document
 
 window.addEventListener('scroll', () => {
   const header = document.querySelector('header')
@@ -20,4 +13,27 @@ toggleButton.addEventListener('click', () => {
 })
 navbarLinks.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
+})
+
+const techIcons = [
+  'icon-1-HTML5.jpg',
+  'icon-1-CSS3.jpg',
+  'icon-1-Javascript.png',
+  'icon-1-Git.png',
+  'icon-1-DevTools.png',
+  'icon-1-Wireframing.png',
+  'icon-2-es6.png',
+  'icon-2-NPM.png',
+  'icon-2-OOP.png',
+  'icon-2-PostgreSQL.jpg',
+  'icon-2-Node.png',
+  'icon-2-APIIntegrations.png',
+  'icon-3-React.png',
+  'icon-3-Webpack.png'
+]
+techIcons.forEach(x => {
+  const containerTech = document.querySelector('.container-tech')
+  const iconImg = document.createElement('img')
+  iconImg.src = 'assets/icons/' + x
+  containerTech.append(iconImg)
 })
