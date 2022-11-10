a8518f13eadafe4ef49c1a741218f4671950744a
do-jonathan4 <d.johnt45@gmail.com>
2022-11-09 17:11:14 -0800

    fixed about spacing, added projects array

--- main.js
+++ main.js
@@ -37,3 +37,48 @@ techIcons.forEach(x => {
   iconImg.src = 'assets/icons/' + x
   containerTech.append(iconImg)
 })
+
+const projectsArray = [
+  {
+    img: 'ajax-project.jpg',
+    title: 'Chuck Norris Facts',
+    link: 'https://do-jonathan4.github.io/ajax-project/',
+    git: 'https://github.com/do-jonathan4/ajax-project'
+  },
+  {
+    img: 'week-planner.jpg',
+    title: 'Week Planner',
+    link: 'https://do-jonathan4.github.io/week-planner/',
+    git: 'https://github.com/do-jonathan4/week-planner'
+  },
+  {
+    img: 'chat-app-project.jpg',
+    title: 'Group Chat App',
+    link: 'https://group-chat-app1.herokuapp.com/',
+    git: 'https://github.com/do-jonathan4/chat-app-project'
+  },
+  {
+    img: 'memory-match.jpg',
+    title: 'Memory Match Game',
+    link: 'https://do-jonathan4.github.io/memory-match/',
+    git: 'https://github.com/do-jonathan4/memory-match'
+  },
+  {
+    img: 'number-guesser.jpg',
+    title: 'Number Guesser Game',
+    link: 'https://do-jonathan4.github.io/number-guesser/',
+    git: 'https://github.com/do-jonathan4/number-guesser'
+  },
+  {
+    img: 'meme-gallery.jpg',
+    title: 'Meme Gallery',
+    link: 'https://do-jonathan4.github.io/meme-gallery/',
+    git: 'https://github.com/do-jonathan4/meme-gallery'
+  },
+  {
+    img: 'character-count-app.jpg',
+    title: 'Character Count App',
+    link: 'https://do-jonathan4.github.io/character-count-app/',
+    git: 'https://github.com/do-jonathan4/character-count-app'
+  }
+]
